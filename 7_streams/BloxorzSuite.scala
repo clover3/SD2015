f220f1abc3d6044213bb6625ec055a0a2002d281
cloer3 <lesterny@gmail.com>
2015-10-07 14:07:13 +0900

    Add test cases

--- 7_streams/BloxorzSuite.scala
+++ 7_streams/BloxorzSuite.scala
@@ -50,6 +50,7 @@ class BloxorzSuite extends FunSuite {
   test("findChar level 1") {
     new Level1 {
       assert(startPos == Pos(1,1))
+      assert(goal == Pos(4,7))
     }
   }
 
@@ -64,4 +65,85 @@ class BloxorzSuite extends FunSuite {
       assert(solution.length == optsolution.length)
     }
   }
+
+  /*
+  trait LevelBug extends SolutionChecker {
+    /* terrain for level 1*/
+
+    val level =
+      """ooo-------
+      |oS-ooo----
+      |oo-oooooo-
+      |-o-ooooooo
+      |-----ooToo
+      |------ooo-""".stripMargin
+
+    val optsolution = Nil
+  }
+test( "solution for levelBug") {
+    new LevelBug {
+      assert((solution) == Nil)
+      assert(solve(solution) == Block(goal, goal))
+    }
+  }
+
+*/
+
+  trait Level3_1 extends SolutionChecker {
+    /* terrain for level 1*/
+    val level =
+      """oooooooooo
+      |oooooooooo
+      |ooooSooToo
+      |oooooooooo
+      |oooooooooo
+      |oooooooooo""".stripMargin
+
+    val optsolution = List(Right, Right)
+  }
+
+  trait Level3_2 extends SolutionChecker {
+    /* terrain for level 1*/
+    val level =
+      """oooooooooo
+        |oooooooooo
+        |oTooSooooo
+        |oooooooooo
+        |oooooooooo
+        |oooooooooo""".stripMargin
+
+    val optsolution = List(Left, Left)
+  }
+
+  trait Level3_3 extends SolutionChecker {
+    /* terrain for level 1*/
+    val level =
+      """oooooooooo
+        |oooooooooo
+        |ooooSooooo
+        |oooooooooo
+        |oooooooooo
+        |ooooTooooo""".stripMargin
+
+    val optsolution = List(Down, Down)
+  }
+
+  test( "solution for Level3") {
+    new Level3_1{
+      assert(solve(solution) === Block(goal, goal))
+      assert(solution.length === 2)
+      assert(solution === optsolution)
+    }
+    new Level3_2{
+      assert(solve(solution) === Block(goal, goal))
+      assert(solution.length === 2)
+      assert(solution === optsolution)
+    }
+    new Level3_3{
+      assert(solve(solution) === Block(goal, goal))
+      assert(solution.length === 2)
+      assert(solution === optsolution)
+    }
+  }
+
 }
